7ff2c3b1266146fc78c5154f60d9f915978e1606
akskokki <akseli.kokki@gmail.com>
2023-02-13 16:35:37 +0200

    Add counters for threads and replies

--- threads.py
+++ threads.py
@@ -3,7 +3,11 @@ from sqlalchemy.sql import text
 import users
 
 def get_list(topic_id):
-    sql = text("SELECT id, title FROM threads WHERE topic_id=:topic_id ORDER BY id")
+    sql = text("SELECT T.id, T.title, COUNT(R.id) reply_count "\
+               "FROM threads T LEFT JOIN replies R ON T.id=R.thread_id "\
+               "WHERE topic_id=:topic_id "\
+               "GROUP BY T.id "\
+               "ORDER BY T.id")
     result = db.session.execute(sql, {"topic_id": topic_id})
     return result.fetchall()
 
--- topics.py
+++ topics.py
@@ -2,7 +2,10 @@ from db import db
 from sqlalchemy.sql import text
 
 def get_list():
-    sql = text("SELECT id, title FROM topics ORDER BY id")
+    sql = text("SELECT O.id, O.title, COUNT(H.id) thread_count "\
+               "FROM topics O LEFT JOIN threads H ON O.id=H.topic_id "\
+               "GROUP BY O.id "\
+               "ORDER BY O.id")
     result = db.session.execute(sql)
     return result.fetchall()
 
