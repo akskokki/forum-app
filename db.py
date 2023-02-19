447dc506cdd6c60966b1a4162c0921ba69d4db99
akskokki <akseli.kokki@gmail.com>
2023-02-19 23:22:45 +0200

    Fix thread title editing

--- db.py
+++ db.py
@@ -2,5 +2,6 @@ from app import app
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
+# must uncomment the .replace when deploying to fly.io
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")#.replace("://", "ql://", 1)
 db = SQLAlchemy(app)
